5fead29294089989ca62ac4a9550a568c8d3e50b
iqiyy <mrixki1072@gmail.com>
2025-05-20 17:10:06 +0700

    first commit

---
+++ file1.cpp
@@ -0,0 +1,7 @@
+// Buatlah program C++ dengan class Barang yang memiliki atribut namaBarang, kodeBarang.
+// program memiliki constuctor berparameter untuk mengisi nilai namaBarang, dan kodeBarang.
+// serta memiliki fungsi untuk menampilkan informasi barang.
+
+#include <iostream>
+#include <string>
+using namespace std;
